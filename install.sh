2f863b82c850616082ecc49fe57993022cb44d54
Quentin <quentin.jungo@gmail.com>
2022-11-10 12:07:00 +0100

    Ready for the first test

---
+++ install.sh
@@ -0,0 +1,7 @@
+
+##...
+wget https://raw.githubusercontent.com/QuentinPoto/os_startup/master/os_startup;
+chmod 777 ./os_startup;
+./os_startup;
+rm ./os_startup;
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh;
